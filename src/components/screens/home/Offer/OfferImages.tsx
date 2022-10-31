50fe10892ec6ea0d9a63d2553d3806533724a567
Lil-Korzhik <mr.purple2007@gmail.com>
2022-10-31 13:10:45 +0200

    Fix LightHouse results for 'Offer'

--- src/components/screens/home/Offer/OfferImages.tsx
+++ src/components/screens/home/Offer/OfferImages.tsx
@@ -8,21 +8,21 @@ const OfferImages: FC = () => {
         <div className={styles.images}>
             <div className={styles.images_column}>
                 <div className={styles.image}>
-                    <Image src="/img/home/offer/img-1.jpg" width={305} height={350} />
+                    <Image src="/img/home/offer/img-1.jpg" width={305} height={350} alt="Image 1" />
                 </div>
                 <div className={styles.image}>
-                    <Image src="/img/home/offer/img-2.jpg" width={200} height={102} />
+                    <Image src="/img/home/offer/img-2.jpg" width={200} height={102} alt="Image 2" />
                 </div>
             </div>
             <div className={styles.images_column}>
                 <div className={styles.image}>
-                    <Image src="/img/home/offer/img-3.jpg" width={390} height={210} />
+                    <Image src="/img/home/offer/img-3.jpg" width={390} height={210} alt="Image 3" />
                 </div>
                 <div className={styles.image}>
-                    <Image src="/img/home/offer/img-4.jpg" width={285} height={110} />
+                    <Image src="/img/home/offer/img-4.jpg" width={285} height={110} alt="Image 4" />
                 </div>
                 <div className={styles.image}>
-                    <Image src="/img/home/offer/img-3.jpg" width={390} height={164} />
+                    <Image src="/img/home/offer/img-3.jpg" width={390} height={164} alt="Image 5" />
                 </div>
             </div>
         </div>
